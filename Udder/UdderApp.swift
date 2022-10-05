24d3755a00797a674ba2332d7ffc4ef21103b9cf
bekturmamytov <bekturmamytov@gmail.com>
2022-10-05 12:24:04 +0900

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Bektur Mamytov on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
